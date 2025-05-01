58eaf32f1eb92523f2cf6f343205930b498dae53
Jakob Kirkegaard Wibroe <jkwibroe91@gmail.com>
2025-05-01 10:10:45 +0200

    Kæmpe ændring. Tilføjet logik til inaktivering af indsatser og forbedret håndtering af forløbsindplacering i process_workqueue.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import sys
 
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 from odk_tools.tracking import Tracker
 from automation_server_client import (
@@ -18,6 +18,7 @@ from kmd_nexus_client import (
     OrganizationsClient,
     CalendarClient,
     AssignmentsClient,
+    GrantsClient,
     filter_references,
 )
 
@@ -54,6 +55,10 @@ async def populate_queue(workqueue: Workqueue):
         
         for borger in borgere:
             try:
+                # skip Nancy
+                if borger["patientIdentifier"]["identifier"] == "251248-9996":
+                    continue
+
                 cpr = borger["patientIdentifier"]["identifier"]
                 workqueue.add_item({}, cpr)
             except Exception as e:
@@ -83,21 +88,39 @@ async def process_workqueue(workqueue: Workqueue):
                 )
 
                 # Finder borgers forløbsindplacering
-                forløbsindplacering_reference = next(
+                forløbsindplacering_grundforløb = next(
                     (ref for ref in basket_grant_references if ref["name"] == "ÆHF - Forløbsindplacering (Grundforløb)"),
                     None
                 )
-                # KAN DETTE LAVES PÆNERE?? #
-                forløbsindplacering_navn = (
-                    forløbsindplacering_reference["children"][0]["children"][0]["children"][0]["name"]
-                    if forløbsindplacering_reference
+                forløbsindplacering_raw = (
+                    forløbsindplacering_grundforløb["children"][0]["children"][0]["children"][0]
+                    if forløbsindplacering_grundforløb
                     else None
+                )
+
+                # Pakker forløbsindplacering ud for at få nuværende opgaver. Skipper borger hvis der allerede er oprettet en opgave på forløbsindplacering
+                resolved_forløbsindplacering_grundforløb = nexus_borgere.resolve_reference(forløbsindplacering_raw)
+                forløbsindplacering_opgaver = nexus_opgaver.get_assignments(resolved_forløbsindplacering_grundforløb)
+                if any(opgaver.get("title") == "testopgave fra rpa" for opgaver in forløbsindplacering_opgaver):
+                    # Hvis der allerede er oprettet en opgave, så spring denne borger over
+                    continue
+
+                # # KAN DETTE LAVES PÆNERE?? #
+                # forløbsindplacering_navn = (
+                #     forløbsindplacering_grundforløb["children"][0]["children"][0]["children"][0]["name"]
+                #     if forløbsindplacering_grundforløb
+                #     else None
+                # )
+
+                matchende_forløbsindplacering = next(
+                    (f for f in forløbsindplacering if f["navn"] == forløbsindplacering_raw["name"]),
+                    None
                 )
 
                 # Henter borgers kalender
                 borger_kalender = nexus_kalender.get_citizen_calendar(borger)
                 borger_kalender_begivenheder = nexus_kalender.events(
-                    borger_kalender, date.today(), date.today() + timedelta(weeks=26)
+                    borger_kalender, date.today() + timedelta(days=2), date.today() + timedelta(weeks=26)
                 )
 
                 # Gennemgår borgers indsatser og ignorer ikke godkendte indsatser
@@ -112,10 +135,11 @@ async def process_workqueue(workqueue: Workqueue):
                     resolved_reference = nexus_borgere.resolve_reference(reference)
                     
                     # Checker indsatsens opgaver og skipper den, hvis der allerede er oprettet en opgave på den indsats
-                    referencens_opgaver = nexus_opgaver.get_assignments(resolved_reference)
-                    if any(opgaver.get("title") == "testopgave fra rpa" for opgaver in referencens_opgaver):
-                        # Hvis der allerede er oprettet en opgave, så spring denne indsats over
-                        continue
+                    # TODO ændr references_opgaver til forløbsindplaceringens opgaver
+                    # referencens_opgaver = nexus_opgaver.get_assignments(resolved_reference)
+                    # if any(opgaver.get("title") == "testopgave fra rpa" for opgaver in referencens_opgaver):
+                    #     # Hvis der allerede er oprettet en opgave, så spring denne indsats over
+                    #     continue
 
                     # Finder id
                     nuværende_bestilling = nexusklient.get(
@@ -138,36 +162,37 @@ async def process_workqueue(workqueue: Workqueue):
                         continue
 
                     # Hvis der ikke er en begivenhed, så find forløbsindplacering. Hvis der ikke er en forløbsindplacering, så sæt matchende_indsats["ansvarlig_organisation"] til "Sygeplejerådgivere fysisk"
-                    if forløbsindplacering_navn is None:
+                    if forløbsindplacering_raw is None:
                         matchende_forløbsindplacering = {
                             "ansvarlig_organisation": "Sygeplejerådgivere fysisk"
                         }
-                    else:
-                        matchende_forløbsindplacering = next(
-                            (f for f in forløbsindplacering if f["navn"] == forløbsindplacering_navn),
-                            None
-                        )
+                    # else:
+                    #     matchende_forløbsindplacering = next(
+                    #         (f for f in forløbsindplacering if f["navn"] == forløbsindplacering_navn),
+                    #         None
+                    #     )
 
 
                     print(matchende_forløbsindplacering["ansvarlig_organisation"])
+                    inaktiver_indsats(borger, resolved_reference)
                     # Opret opgave
-                    opret_opgave = nexus_opgaver.create_assignment(
-                        object=resolved_reference,
-                        assignment_type="Tværfagligt samarbejde",
-                        title="testopgave fra rpa",
-                        responsible_organization=matchende_forløbsindplacering["ansvarlig_organisation"],
-                        responsible_worker=None,
-                        description=None,
-                        start_date=date.today(),
-                        due_date=date.today()
-                    )
+                    # opret_opgave = nexus_opgaver.create_assignment(
+                    #     object=resolved_reference,
+                    #     assignment_type="Tværfagligt samarbejde",
+                    #     title="testopgave fra rpa",
+                    #     responsible_organization=matchende_forløbsindplacering["ansvarlig_organisation"],
+                    #     responsible_worker=None,
+                    #     description=None,
+                    #     start_date=date.today(),
+                    #     due_date=date.today()
+                    # )
 
-                    # Afregn opgave
-                    if(opret_opgave):
-                        afregningsklient.track_task("Slutlister sygepleje")
-                        logger.info(
-                            f"Opgave oprettet for borger {item.reference} med på indsats {reference['name']}"
-                        )
+                    # # Afregn opgave
+                    # if(opret_opgave):
+                    #     # afregningsklient.track_task("Slutlister sygepleje")
+                    #     logger.info(
+                    #         f"Opgave oprettet for borger {item.reference} med på indsats {reference['name']}"
+                    #     )
 
                 print("stop")
                 pass
@@ -176,6 +201,37 @@ async def process_workqueue(workqueue: Workqueue):
                 logger.error(f"Fejl ved processering af item: {data}. Fejl: {e}")
                 item.fail(str(e))
 
+
+def inaktiver_indsats(borger: dict, resolved_indsats: dict):
+    """
+    Inaktiverer indsats for borger.
+    param borger: dict: Borgerens data
+    param resolved_indsats: dict: Indsatsens data
+    """
+
+    transitions = {"Bestilt": "Afslut", "Bevilliget": "Annullér", "Ændret": "Afslut"}
+
+    if resolved_indsats["workflowState"]["name"] not in transitions:
+        raise WorkItemError(
+            f"Kan ikke afslutte indsats på borger {borger["patientIdentifier"]["identifier"]} med indsats {resolved_indsats["name"]} da den ikke er i en gyldig tilstand."
+        )
+    
+    opdaterings_felter = {}
+
+    if transitions[resolved_indsats["workflowState"]["name"]] == "Afslut":
+        opdaterings_felter["billingEndDate"] = datetime.now().astimezone().isoformat()
+        opdaterings_felter["basketGrantEndDate"] = datetime.now().astimezone().isoformat()
+    else: 
+        opdaterings_felter["cancelledDate"] = datetime.now().astimezone().isoformat()
+
+    try:
+        nexus_indsatser.edit_grant(
+            grant=resolved_indsats,changes=opdaterings_felter, transition=transitions[resolved_indsats["workflowState"]["name"]]
+        )
+    except Exception as e:
+        raise WorkItemError(
+            f"Fejl ved inaktivering af indsats på borger {borger['patientIdentifier']['identifier']} med indsats {resolved_indsats['name']}: {e}"
+        )
 
 if __name__ == "__main__":
     ats = AutomationServer.from_environment()
@@ -198,6 +254,7 @@ if __name__ == "__main__":
         nexus_client=nexusklient, citizens_client=nexus_borgere
     )
     nexus_opgaver = AssignmentsClient(nexus_client=nexusklient)
+    nexus_indsatser = GrantsClient(nexus_client=nexusklient)
 
     afregningsklient = Tracker(
         username=afregnings_credential.username, password=afregnings_credential.password
